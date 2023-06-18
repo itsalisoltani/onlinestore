13ac6fec2dcf5d6839d00293c1f4fa2dd0ca5e24
Tony Soltani <its.alisoltani@gmail.com>
2023-06-18 15:44:23 +0330

    relocate index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["./src/index.html"],
+  content: ["./dist/index.html"],
   theme: {
     extend: {
       colors: {
